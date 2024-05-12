c92486fb11f578a8809993dabf735ddb97488045
DingX2 <pourding@naver.com>
2024-05-12 15:23:33 +0900

    feat(card): card 컴포넌트 구현

---
+++ src/components/molecules/Card.ts
@@ -0,0 +1,36 @@
+import type { ParentComponent } from 'solid-js';
+import { styled } from 'solid-styled-components';
+
+interface Props {
+    width?: number;
+    height?: number;
+    top?: boolean;
+    middle?: boolean;
+    bottom?: boolean;
+}
+
+export const Card: ParentComponent<Props> = styled.div`
+    width: ${({ width }) => width || 300}px;
+    height: ${({ height }) => height || 300}px;
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(2.5px);
+    -webkit-backdrop-filter: blur(2.5px);
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+
+    display: flex;
+    flex-direction: column;
+    ${({ top, middle, bottom }) => {
+        if (top) {
+            return 'justify-content: flex-start;';
+        }
+        if (middle) {
+            return 'justify-content: center;';
+        }
+        if (bottom) {
+            return 'justify-content: flex-end;';
+        }
+        return '';
+    }};
+`;
