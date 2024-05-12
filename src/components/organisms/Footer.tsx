40f15862d793e2743164dcfdffa04ff29f7773cf
DingX2 <pourding@naver.com>
2024-05-12 15:21:55 +0900

    feat(footer): footer 구현

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,5 +1,13 @@
 import type { Component } from 'solid-js';
+import { Text, Stack, Link } from '@/components/atoms';
 
 export const Footer: Component = () => {
-    return <footer>footer</footer>;
+    return (
+        <Stack direction="row" alignItemCenter center spacing={10}>
+            <Link href="http://localhost:5173/contact" target="_blank">
+                {'MIT License'}
+            </Link>
+            <Text textAlign="center">SooHyeon Lee © 2024</Text>
+        </Stack>
+    );
 };
