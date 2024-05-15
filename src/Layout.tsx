c90be8a711c1276d34a0b00d41a378f3cffb3260
DingX2 <pourding@naver.com>
2024-05-15 23:30:28 +0900

    fix(layout): path alias 수정

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,6 @@
 import type { ParentComponent } from 'solid-js';
 import { GlobalStyle } from '@/components/common';
-import { Header, Footer } from './components/organisms';
+import { Header, Footer } from '@/components/organisms';
 
 export const Layout: ParentComponent = ({ children }) => {
     return (
