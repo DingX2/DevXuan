246651196a12a31eaeea5b4885cfeff33cf6bc8e
DingX2 <pourding@naver.com>
2024-05-21 01:06:08 +0900

    feat(skills): skills 더미데이터 생성

---
+++ src/constants/skills.ts
@@ -0,0 +1,124 @@
+export const skills = [
+    {
+        language: [
+            {
+                name: 'Typescript',
+                image: '/image/Typescript_logo.svg',
+                description: '암묵적 타입 변환으로 버그가 발생한 경험으로 전환했어요',
+                level: 3,
+            },
+            {
+                name: 'JavaScript',
+                image: '/image/JavaScript_logo.svg',
+                description: '다양한 메소드를 지원해요',
+                level: 3,
+            },
+            {
+                name: 'Python',
+                image: '/image/JavaScript_logo.svg',
+                description: '간단한 웹서버는 만들 수 있어요',
+                level: 2,
+            },
+            {
+                name: 'Java',
+                image: '/image/JavaScript_logo.svg',
+                description: '기본적인 문법을 알아요',
+                level: 1,
+            },
+            {
+                name: 'Kotlin',
+                image: '/image/JavaScript_logo.svg',
+                description: '앱개발에 사용해봤어요',
+                level: 1,
+            },
+            {
+                name: 'C',
+                image: '/image/JavaScript_logo.svg',
+                description: 'C-family 언어에 빠르게 적응할 수 있어요',
+                level: 1,
+            },
+        ],
+        library: [
+            {
+                name: 'React.js',
+                image: '/image/Typescript_logo.svg',
+                description: '다양한 라이브러리',
+                level: 3,
+            },
+            {
+                name: 'Zustand',
+                image: '/image/JavaScript_logo.svg',
+                description: 'C-family 언어에 빠르게 적응할 수 있어요',
+                level: 1,
+            },
+            {
+                name: 'React-query',
+                image: '/image/JavaScript_logo.svg',
+                description: 'C-family 언어에 빠르게 적응할 수 있어요',
+                level: 1,
+            },
+            {
+                name: 'Zustand',
+                image: '/image/JavaScript_logo.svg',
+            },
+            {
+                name: 'Zustand',
+                image: '/image/JavaScript_logo.svg',
+            },
+        ],
+        css: [
+            {
+                name: 'Emotion',
+                image: '/image/Typescript_logo.svg',
+            },
+            {
+                name: 'styled-components',
+                image: '/image/JavaScript_logo.svg',
+            },
+            {
+                name: 'sass',
+                image: '/image/JavaScript_logo.svg',
+            },
+            {
+                name: 'tailwind',
+                image: '/image/JavaScript_logo.svg',
+            },
+        ],
+        tooling: [
+            {
+                name: 'Github',
+                image: '/image/Typescript_logo.svg',
+            },
+            {
+                name: 'Notion',
+                image: '/image/JavaScript_logo.svg',
+            },
+            {
+                name: 'Figma',
+                image: '/image/JavaScript_logo.svg',
+            },
+            {
+                name: 'Jira',
+                image: '/image/JavaScript_logo.svg',
+            },
+            {
+                name: 'slack',
+                image: '/',
+            },
+        ],
+        Server: [
+            {
+                name: 'AWS',
+                image: '/image/Typescript_logo.svg',
+            },
+            {
+                name: 'Nginx',
+                image: '/image/JavaScript_logo.svg',
+            },
+            {
+                name: 'Netlify',
+                image: '/image/JavaScript_logo.svg',
+            },
+        ],
+    },
+];
