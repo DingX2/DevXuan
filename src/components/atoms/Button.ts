4be9d774af1884de4e6eb2349e8b7e15091f057d
DingX2 <pourding@naver.com>
2024-05-20 14:10:39 +0900

    feat(button): 이미지 버튼 옵션 추가

--- src/components/atoms/Button.ts
+++ src/components/atoms/Button.ts
@@ -3,14 +3,35 @@ import { styled } from 'solid-styled-components';
 interface Props {
     backgroundColor?: string;
     color?: string;
-    size?: number;
+    width?: string;
+    height?: string;
     fontSize?: number;
     link?: string;
+    icon?: string;
 }
 
+/**
+ * @param {string} backgroundColor - background-color
+ * @param {string} color - color
+ * @param {string} width - width 100% 기본
+ * @param {string} height - height auto 기본
+ * @param {number} fontSize - font-size
+ * @param {string} link - link
+ * @param {string} icon - 이미지 버튼, 24px 기본
+ */
+
 export const Button = styled.button<Props>`
+    background-image: ${({ icon }) => (icon ? `url(${icon})` : 'none')};
+    background-size: 1.5rem;
+    background-repeat: no-repeat;
+    background-position: center;
+
+    width: ${({ icon, width = icon ? '24px' : '100%' }) => width};
+    height: ${({ icon, height = icon ? '24px' : 'auto' }) => height};
+    ${({ icon }) => (icon ? 'border: none;' : '')}
+
     color: #000;
-    background: ${({ backgroundColor }) => backgroundColor || '#FF0000'}
+    background-color: ${({ backgroundColor }) => backgroundColor || 'transparent'};
     padding: 0.5rem 1rem;
     border-radius: 5px;
     font-size: ${({ fontSize }) => fontSize};
