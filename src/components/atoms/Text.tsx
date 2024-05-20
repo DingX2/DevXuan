3e8244cf152fe02f6baea2d8704d4f64021c2378
DingX2 <pourding@naver.com>
2024-05-20 14:13:53 +0900

    feat(text): text sx 옵션 추가

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -1,5 +1,6 @@
 import type { ParentComponent } from 'solid-js';
 import { styled } from 'solid-styled-components';
+import type { Style } from '@/types';
 
 interface PositionSelector {
     [key: string]: string;
@@ -16,13 +17,20 @@ const positionSelector: PositionSelector = {
 
 type ComponentType = keyof HTMLElementTagNameMap;
 
-interface Props {
+interface Props extends Style {
     color?: string;
     fontSize?: string;
     textAlign?: 'start' | 'center' | 'end' | 'justify';
     position?: 'left' | 'center' | 'right';
     component?: ComponentType;
 }
+
+/**
+ * @param {string} color - color
+ * @param {string} fontSize - font-size
+ * @param {'start' | 'center' | 'end' | 'justify'} textAlign - text-align
+ * @param {'left' | 'center' | 'right'} position - position
+ */
 
 export const Text: ParentComponent<Props> = ({
     component = 'span',
@@ -31,15 +39,18 @@ export const Text: ParentComponent<Props> = ({
     fontSize,
     textAlign,
     position,
+    sx,
 }) => {
     const TextComponent = styled(component ?? 'h1')`
         display: flex;
+        display: inline-block;
         ${color ? `color: ${color};` : ''}
         ${textAlign ? `text-align: ${textAlign};` : ''}
         ${position ? `justify-content: ${positionSelector[position]};` : ''}
-
+    
         white-space: pre-line;
         font-size: ${fontSize || '16px'};
+        ${sx || ''}
     `;
 
     return <TextComponent>{children}</TextComponent>;
