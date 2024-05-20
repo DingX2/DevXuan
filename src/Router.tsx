3a30f8db19eec5303413248f88ae7baf840732d6
DingX2 <pourding@naver.com>
2024-05-21 01:42:13 +0900

    refactor(router): router 재설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import type { VoidComponent } from 'solid-js';
 import { Router as SolidRouter, Route } from '@solidjs/router';
-import { Home, NotFound, About, Skill, Work, Contact, Test } from '@/pages';
+import { Home, NotFound, About, Skill, Work, Blog, Test } from '@/pages';
 import { Layout } from './Layout';
 
 export const Router: VoidComponent = () => {
@@ -10,7 +10,7 @@ export const Router: VoidComponent = () => {
             <Route path="/about" component={About} />
             <Route path="/skill" component={Skill} />
             <Route path="/work" component={Work} />
-            <Route path="/contact" component={Contact} />
+            <Route path="/blog" component={Blog} />
             <Route path="/test" component={Test} />
             <Route path="/*" component={NotFound} />
         </SolidRouter>
