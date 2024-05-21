c1c17deb89964d90b23faf0361ba3195b22d1fa9
DingX2 <pourding@naver.com>
2024-05-21 20:01:16 +0900

    feat(profilecard): 프로필카드 구현 모션필요

--- src/components/organisms/ProfileCard.tsx
+++ src/components/organisms/ProfileCard.tsx
@@ -1,47 +1,54 @@
-import type { ParentComponent } from 'solid-js';
+import { type ParentComponent, createSignal } from 'solid-js';
 import { styled } from 'solid-styled-components';
 import { Stack, Text, Image } from '@/components/atoms';
-import { Card } from '@/components/molecules';
-import profile from '@/assets/profile.png';
+import { Card, Background, Nav } from '@/components/molecules';
 
 interface Props {}
 
 export const ProfileCard: ParentComponent<Props> = ({ children }) => {
+    const [flipped, setFlipped] = createSignal(false);
+
+    const handleFlip = () => {
+        setFlipped(!flipped);
+    };
+
     const ProfileCardComponent = styled.div`
         display: flex;
-        margin-top: 2rem;
         justify-content: center;
     `;
 
     return (
-        <ProfileCardComponent>
-            <Card width={1080} height={600} middle>
-                <Stack direction="row">
-                    <Stack direction="column">
-                        <Text component="h1" fontSize="50px">
-                            FrontEnd {'\n'} Developer
-                        </Text>
-                        <Text component="span">DevXuan</Text>
+        <ProfileCardComponent onClick={handleFlip}>
+            <Background height="80" grid />
+            <Card width={80} height={400} middle>
+                <Stack direction="column">
+                    <Nav />
+                    <Stack direction="row" center>
+                        <Stack direction="column">
+                            <Text component="h1" fontSize="50px">
+                                FrontEnd {'\n'} Developer
+                            </Text>
+                            <Text component="span">DevXuan</Text>
+                        </Stack>
+                        <Stack direction="column" center>
+                            <Text component="span" textAlign="end" position="right">
+                                이수현
+                            </Text>
+                            <Text component="p" textAlign="end" position="right">
+                                더 나은 세상을 만들고 싶어요.
+                            </Text>
+                        </Stack>
                     </Stack>
-                    <Stack direction="column">
-                        <Text component="span" textAlign="end" position="right">
-                            이수현
-                        </Text>
-                        <Text component="p" textAlign="end" position="right">
-                            세상을 이롭게 만들고 싶어요
-                        </Text>
-                    </Stack>
+                    <Image
+                        src="/image/profile.png"
+                        alt="profile"
+                        width={300}
+                        useAbsolute
+                        zIndex={100}
+                        bottom={0}
+                        center
+                    />
                 </Stack>
-                <Image
-                    src={profile}
-                    alt="profile"
-                    width={300}
-                    height={200}
-                    useAbsolute
-                    zIndex={100}
-                    bottom={0}
-                    center
-                />
             </Card>
             {children}
         </ProfileCardComponent>
