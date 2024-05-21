5948d539275ce2fdf384051b0ec50c3f04032dcb
DingX2 <pourding@naver.com>
2024-05-21 20:01:47 +0900

    feat(skilltemplate): 기술스택란 1차 구현

--- src/components/templates/SkillTemplate.tsx
+++ src/components/templates/SkillTemplate.tsx
@@ -1,5 +1,30 @@
-import type { Component } from 'solid-js';
+import { type Component, For } from 'solid-js';
+import { Text, Stack } from '@/components/atoms';
+import { ImageBox } from '@/components/molecules';
+import { skills } from '@/constants';
 
 export const SkillTemplate: Component = () => {
-    return <div>SkillTemplate</div>;
+    return (
+        <>
+            <Stack direction="column" useFlexGap spacing={10}>
+                <Text>Skill</Text>
+                <Stack direction="row" useFlexGap spacing={10}>
+                    <ImageBox image="/image/Typescript_logo.svg" width={24} height={24}>
+                        <Text>HTML</Text>
+                    </ImageBox>
+                    <For each={skills}>
+                        {({ language }) => (
+                            <For each={language}>
+                                {({ image, name, description }) => (
+                                    <ImageBox image={image} width={24} height={24} hoverText={description}>
+                                        <Text>{name}</Text>
+                                    </ImageBox>
+                                )}
+                            </For>
+                        )}
+                    </For>
+                </Stack>
+            </Stack>
+        </>
+    );
 };
