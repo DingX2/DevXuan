9746ec8579e522a2392a4d419a525f96417453c9
DingX2 <pourding@naver.com>
2024-05-21 19:58:07 +0900

    feat(background): 기본 CSS, 이미지 변경 기능 구현

--- src/components/molecules/Background.tsx
+++ src/components/molecules/Background.tsx
@@ -1,23 +1,51 @@
-import type { ParentComponent } from 'solid-js';
-import { Stack } from '@/components/atoms';
+import { type ParentComponent, Show } from 'solid-js';
+import { Stack, Image } from '@/components/atoms';
 
-export const Background: ParentComponent = ({ children }) => {
+interface Props {
+    height: string;
+    image?: string;
+    grid?: boolean;
+}
+
+export const Background: ParentComponent<Props> = ({ children, height, image, grid }) => {
     return (
-        <Stack
-            center
-            sx={
-                /* css */ `
-                position: relative;
-                left: 0;
-                width: 100%;
-                z-index: -99;
-                background: linear-gradient(55deg, #FED3F9 0%, #90A3EA 100%),linear-gradient(to bottom, transparent 18px, #efefef 8px) 0 0 / 100vw 19px repeat-y,
-                linear-gradient(to right, transparent 18px, #efefef 8px) 0 0 / 19px 100vh repeat-x;
-                background-blend-mode: color-dodge;
-            `
+        <Show
+            when={grid}
+            fallback={
+                <Image
+                    src={image}
+                    alt="background"
+                    sx={
+                        /* css */ `
+                        position: absolute,
+                        left: 0,
+                        top: 0,
+                        width: 100%,
+                        height: ${height}vh,
+                        z-index: -99,
+                    `
+                    }
+                />
             }
         >
-            <Stack>{children}</Stack>
-        </Stack>
+            <Stack
+                center
+                sx={
+                    /* css */ `
+                    position: absolute;
+                    left: 0;
+                    width: 100%;
+                    height: ${height}vh;
+                    z-index: -99;
+                    background: linear-gradient(55deg, #FED3F9 0%, #90A3EA 100%),
+                    linear-gradient(to bottom, transparent 18px, #efefef 8px) 0 0 / 100vw 19px repeat-y,
+                    linear-gradient(to right, transparent 18px, #efefef 8px) 0 0 / 19px 100vh repeat-x;
+                    background-blend-mode: color-dodge;
+    `
+                }
+            >
+                <Stack>{children}</Stack>
+            </Stack>
+        </Show>
     );
 };
