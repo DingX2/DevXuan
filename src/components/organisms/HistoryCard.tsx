efd7a41c1ef298ad9bf7c047c563c3ad871c04ab
DingX2 <pourding@naver.com>
2024-05-27 13:04:00 +0900

    refactor(historycard): historycard 설계 변경

--- src/components/organisms/HistoryCard.tsx
+++ src/components/organisms/HistoryCard.tsx
@@ -1,122 +1,69 @@
-import { type ParentComponent, createSignal } from 'solid-js';
-import { Motion } from 'solid-motionone';
-import { Image, Box, Text, Stack, Link } from '@/components/atoms';
-import { Speech } from '@/components/molecules';
+import type { ParentComponent } from 'solid-js';
+import type { TextWithColor } from '@/types';
+import { Box, Stack } from '@/components/atoms';
+import { CardHeader, CardContent } from '@/components/molecules';
 
 interface Props {
     title: string;
     subtitle: string;
     projectDuration: string;
+    skills: TextWithColor[];
     content: string;
+    projectImage?: string;
     link?: string;
-    extracurricular: boolean;
-    extraTitle?: string;
-    extraDuration?: string;
-    extraContent?: string;
+    show: boolean;
+    handleClick: () => void;
 }
 
-export const HistoryCard: ParentComponent<Props> = ({
-    children,
-    title,
-    subtitle,
-    projectDuration,
-    content,
-    link,
-    extracurricular,
-    extraTitle,
-    extraDuration,
-    extraContent,
-}) => {
-    const [showDetails, setShowDetails] = createSignal(false);
-
-    const handleClick = () => {
-        console.log('click', showDetails());
-        setShowDetails(!showDetails());
-    };
-
+export const HistoryCard: ParentComponent<Props> = (props) => {
     return (
-        <Stack direction="row" alignItemCenter useFlexGap spacing={10}>
-            {extracurricular && showDetails() ? (
-                <Box width="300px" backgroundColor="#fff" padding="1rem">
-                    <Stack direction="column" center useFlexGap spacing={10}>
-                        <Box backgroundColor="#eee" width="fit-content" padding="0.5rem 1rem;">
-                            <Text>✨ 대외활동</Text>
-                        </Box>
-                        <Text bold>{extraTitle}</Text>
-                        <Text>📅 {extraDuration}</Text>
-                        <Text>💡 {extraContent}</Text>
-                    </Stack>
-                </Box>
-            ) : (
-                ''
-            )}
-            {/* <Card width={40} shadow middle padding="25px 24px 0">
-                <Stack direction="column" useFlexGap spacing={10}>
-                    <Text fontSize="18px">{subtitle}</Text>
-                    <Text fontSize="22px">{title}</Text>
-                    <Text color="#666">{content}</Text>
-                    <Box>
-                        <Image src="/image/typescript_logo.svg" height="166px" sx="margin: 0 -24px;" />
-                    </Box>
-                </Stack>
-            </Card> */}
-            <Image src="/image/dandelion.svg" width={100} height={100} />
-            {showDetails() ? (
-                <Motion
-                    initial={{ opacity: 0, y: 50, scale: 1 }}
-                    animate={{ opacity: 1, y: 0, transition: { delay: 0.05 } }}
-                    transition={{ duration: 0.25, easing: 'ease-out' }}
-                    exit={{ opacity: 0, y: -50, scale: 1 }}
-                >
-                    <Box
-                        width="100%"
-                        backgroundColor="#fff"
-                        direction="column"
-                        useFlexGap
-                        spacing={5}
-                        padding="1rem"
-                        pointer
-                        onClick={handleClick}
-                    >
-                        <Speech>프로젝트</Speech>
-                        <Text fontSize="16px">{subtitle}</Text>
-                        <Text fontSize="18px" bold>
-                            {title}
-                        </Text>
-                        <Text fontSize="12px">📅 {projectDuration}</Text>
-                        <Text color="#666">{content}</Text>
-                        {link && (
-                            <Link href={link} target="_blank">
-                                {link}
-                            </Link>
-                        )}
-                        {children}
-                    </Box>
-                </Motion>
-            ) : (
+        <Stack direction="row" alignItemCenter useFlexGap spacing={10} center>
+            {props.show ? (
                 <Box
-                    width="630px"
                     backgroundColor="#fff"
                     direction="column"
                     useFlexGap
                     spacing={5}
                     padding="1rem"
                     pointer
-                    onClick={handleClick}
+                    onClick={props.handleClick}
+                    sx={`flex: 1;`}
+                    initial={{ opacity: 0, y: 50, scale: 1 }}
+                    animate={{ opacity: 1, y: 0, transition: { delay: 0.05 } }}
+                    transition={{ duration: 0.25, easing: 'ease-out' }}
+                    exit={{ opacity: 0, y: -50, scale: 1 }}
                 >
-                    <Speech>자세히보기</Speech>
-                    <Text fontSize="16px">{subtitle}</Text>
-                    <Text fontSize="18px" bold>
-                        {title}
-                    </Text>
-                    <Text fontSize="12px">📅 {projectDuration}</Text>
-                    <Text color="#666">{content}</Text>
-                    {link && (
-                        <Link href={link} target="_blank">
-                            {link}
-                        </Link>
-                    )}
-                    {children}
+                    <CardHeader
+                        category="자세히보기"
+                        subtitle={props.subtitle}
+                        title={props.title}
+                        duration={props.projectDuration}
+                        skills={props.skills}
+                    />
+
+                    <CardContent content={props.content} projectImage={props.projectImage} link={props.link} />
+
+                    {props.children}
+                </Box>
+            ) : (
+                <Box
+                    backgroundColor="#fff"
+                    direction="column"
+                    useFlexGap
+                    spacing={5}
+                    padding="1rem"
+                    pointer
+                    onClick={props.handleClick}
+                >
+                    <CardHeader
+                        category="프로젝트"
+                        subtitle={props.subtitle}
+                        title={props.title}
+                        duration={props.projectDuration}
+                        skills={props.skills}
+                    />
+
+                    <CardContent content={props.content} projectImage={props.projectImage} link={props.link} />
                 </Box>
             )}
         </Stack>
