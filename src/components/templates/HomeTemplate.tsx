4c0650970b81ce9a09ab4ec3c9cdfe930458a900
DingX2 <pourding@naver.com>
2024-05-28 18:07:39 +0900

    refactor(hometemplate): svg로 전환

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,60 +1,46 @@
-import type { Component } from 'solid-js';
+import { type Component, For } from 'solid-js';
 import { Image, Stack, Text } from '@/components/atoms';
 import { Background } from '@/components/molecules';
 import { ProfileCard } from '@/components/organisms';
-import { backgrounds } from '@/constants';
+import { backgrounds, homeImages } from '@/constants';
 
 export const HomeTemplate: Component = () => {
     return (
         <>
-            <Image
-                src="/image/computer.svg"
-                width={280}
-                useAbsolute
-                sx="
-                    transform: translate(-50%, -50%) rotate(-15deg);
-                    top: 36%;
-                    left: 38%;"
-                zIndex={1}
-            />
-            <Image
-                src="/image/coffee.svg"
-                width={250}
-                useAbsolute
-                sx="
-                    transform: translate(-50%, -50%) rotate(-15deg);
-                    top: 80%;
-                    left: 90%;"
-            />
-            <Image
-                src="/image/camera.svg"
-                width={150}
-                useAbsolute
-                sx="
-                    transform: translate(-50%, -50%) rotate(55deg);
-                    top: 33%;
-                    left: 70%;"
-            />
-            <Background height="100" select sx={backgrounds.gradient} />
-            <ProfileCard>
-                <Stack direction="row">
-                    <Stack direction="column" useFlexGap spacing={10}>
-                        <Text component="h1" fontSize="50px">
-                            FrontEnd {'\n'} Developer
-                        </Text>
-                        <Text component="span">@DevXuan</Text>
+            <Background height="100" select sx={`${backgrounds.gradient} position: relative; padding-bottom: 5rem;`}>
+                <For each={homeImages}>
+                    {({ src, width, sx, zIndex }) => (
+                        <Image src={src} width={width} sx={sx} zIndex={zIndex} useAbsolute />
+                    )}
+                </For>
+                <ProfileCard>
+                    <Stack direction="row">
+                        <Stack direction="column" useFlexGap spacing={10}>
+                            <Text component="h1" fontSize="50px">
+                                FrontEnd {'\n'} Developer
+                            </Text>
+                            <Text component="span">@DevXuan</Text>
+                        </Stack>
+                        <Stack direction="column" center>
+                            <Text component="span" textAlign="end" position="right" fontSize="30px">
+                                이수현
+                            </Text>
+                            <Text component="p" textAlign="end" position="right">
+                                더 나은 세상을 만들고 싶어요.
+                            </Text>
+                        </Stack>
                     </Stack>
-                    <Stack direction="column" center>
-                        <Text component="span" textAlign="end" position="right" fontSize="30px">
-                            이수현
-                        </Text>
-                        <Text component="p" textAlign="end" position="right">
-                            더 나은 세상을 만들고 싶어요.
-                        </Text>
-                    </Stack>
-                </Stack>
-                <Image src="/image/profile.png" alt="profile" width={300} useAbsolute zIndex={100} bottom={0} center />
-            </ProfileCard>
+                    <Image
+                        src="/image/profile.png"
+                        alt="profile"
+                        width={300}
+                        useAbsolute
+                        zIndex={100}
+                        bottom={0}
+                        center
+                    />
+                </ProfileCard>
+            </Background>
         </>
     );
 };
---
+++ src/constants/homeImage.ts
@@ -0,0 +1,102 @@
+export const homeImages = [
+    {
+        src: '/image/home/computer.svg',
+        width: 200,
+        zIndex: 1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(-15deg);
+            top: 46%;
+            left: 42%;
+        `,
+    },
+    {
+        src: '/image/home/coffee.svg',
+        width: 100,
+        zIndex: 1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(-15deg);
+            top: 55%;
+            left: 62%;
+        `,
+    },
+    {
+        src: '/image/home/camera.svg',
+        width: 100,
+        zIndex: 1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(55deg);
+            top: 35%;
+            left: 62%;
+        `,
+    },
+    {
+        src: '/image/home/star.svg',
+        width: 30,
+        zIndex: 1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(55deg);
+            top: 45%;
+            left: 65%;
+        `,
+    },
+    {
+        src: '/image/home/star.svg',
+        width: 20,
+        zIndex: 1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(55deg);
+            top: 35%;
+            left: 55%;
+        `,
+    },
+    {
+        src: '/image/home/game.svg',
+        width: 130,
+        zIndex: 1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(10deg);
+            top: 35%;
+            left: 50%;
+        `,
+    },
+    {
+        src: '/image/home/liquid.svg',
+        width: 350,
+        zIndex: -1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(10deg);
+            top: 50%;
+            left: 45%;
+        `,
+    },
+    {
+        src: '/image/home/liquid.svg',
+        width: 200,
+        zIndex: -1,
+        sx: `
+            transform: translate(-50%, -0%) rotate(140deg);
+            top: 50%;
+            left: 70%;
+        `,
+    },
+    {
+        src: '/image/home/cloud.svg',
+        width: 300,
+        zIndex: -1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(0deg);
+            top: 80%;
+            left: 86%;
+        `,
+    },
+    {
+        src: '/image/home/cloud2.svg',
+        width: 200,
+        zIndex: -1,
+        sx: `
+            transform: translate(-50%, -50%) rotate(0deg);
+            top: 30%;
+            left: 10%;
+        `,
+    },
+];
