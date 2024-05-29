2914d26cc51420593806eb2c82ee3dcb3650ed77
DingX2 <pourding@naver.com>
2024-05-29 18:16:35 +0900

    feat(categorycard): title이 없는 경우 추가

--- src/components/organisms/CategoryCard.tsx
+++ src/components/organisms/CategoryCard.tsx
@@ -3,7 +3,8 @@ import { Box, Stack, Text } from '@/components/atoms';
 
 interface Props {
     category: string;
-    title: string;
+    organization: string;
+    title: string | null;
     duration: string;
     content: string;
     show: boolean;
@@ -12,12 +13,13 @@ interface Props {
 
 export const CategoryCard: Component<Props> = (props) => {
     return (
-        <Box backgroundColor="#fff" padding="1rem" pointer height="fit-content">
+        <Box backgroundColor="#fff" padding="1rem" pointer height="fit-content" sx="z-index: 20;">
             <Stack direction="column" center useFlexGap spacing={10}>
                 <Box backgroundColor="#eee" width="fit-content" padding="0.5rem 1rem;">
                     <Text>{props.category}</Text>
                 </Box>
-                <Text bold>{props.title}</Text>
+                {props.title && <Text bold>{props.title}</Text>}
+                <Text bold>{props.organization}</Text>
                 <Text fontSize="12px">📅 {props.duration}</Text>
                 <Text fontSize="14px;" color="#666">
                     💡 {props.content}
