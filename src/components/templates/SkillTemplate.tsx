50e1835b500086bb7d196474f1b195899f2be2ac
DingX2 <pourding@naver.com>
2024-05-31 14:37:59 +0900

    feat(skilltemplate): 클릭이벤트 등록

--- src/components/templates/SkillTemplate.tsx
+++ src/components/templates/SkillTemplate.tsx
@@ -1,27 +1,45 @@
-import { type Component, For } from 'solid-js';
+import { type Component, For, createSignal } from 'solid-js';
 import { Text, Stack } from '@/components/atoms';
 import { ImageBox } from '@/components/molecules';
 import { skills, backgrounds } from '@/constants';
 
 export const SkillTemplate: Component = () => {
+    const initialState: boolean[] = Array<boolean>(skills.length).fill(false);
+    const [click, setClick] = createSignal<boolean[]>(initialState);
+
+    const handleClick = (index: number) => {
+        console.log('click:', click());
+        setClick((prev) => {
+            const newClick = [...prev];
+            newClick[index] = !newClick[index];
+            return newClick;
+        });
+    };
+
     return (
-        <>
-            <Stack direction="column" useFlexGap spacing={10} sx={backgrounds.grid}>
-                <Text fontSize="30px">Skill</Text>
-                <Stack direction="row" useFlexGap spacing={10} sx="flex-wrap: wrap;">
-                    <For each={skills}>
-                        {({ language }) => (
-                            <For each={language}>
-                                {({ image, name, description }) => (
-                                    <ImageBox image={image} width={24} height={24} hoverText={description}>
-                                        <Text>{name}</Text>
-                                    </ImageBox>
-                                )}
-                            </For>
-                        )}
-                    </For>
-                </Stack>
+        <Stack direction="column" useFlexGap spacing={10} sx={backgrounds.grid}>
+            <Text fontSize="30px">Skill</Text>
+            <Stack direction="row" useFlexGap spacing={10} sx="flex-wrap: wrap; ">
+                <For each={skills} fallback={<div>Loading...</div>}>
+                    {(skills) => (
+                        <For each={skills.language}>
+                            {(category, index) => (
+                                <ImageBox
+                                    image={category.image}
+                                    width="100"
+                                    imgWidth={24}
+                                    height={24}
+                                    hoverText={category.description}
+                                    onClick={() => handleClick(index())}
+                                    click={click()[index()]}
+                                >
+                                    <Text>{category.name}</Text>
+                                </ImageBox>
+                            )}
+                        </For>
+                    )}
+                </For>
             </Stack>
-        </>
+        </Stack>
     );
 };
