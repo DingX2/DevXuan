733a6788b8c50f7917a2c2978865d75be5b43797
DingX2 <pourding@naver.com>
2024-06-01 08:38:22 +0900

    feat(imagebox): imagebox 클릭 이벤트 구현

--- src/components/molecules/ImageBox.tsx
+++ src/components/molecules/ImageBox.tsx
@@ -1,5 +1,5 @@
 import { type ParentComponent } from 'solid-js';
-import { Image, Box, Text } from '@/components/atoms';
+import { Image, Box, Text, Stack } from '@/components/atoms';
 import { animation } from '@/utils';
 
 interface Props {
@@ -8,22 +8,30 @@ interface Props {
     height?: string | number;
     imgWidth?: string | number;
     hoverText?: string;
+    click?: boolean;
+    onClick?: () => void;
 }
 
-export const ImageBox: ParentComponent<Props> = ({ image, children, width, height, imgWidth, hoverText }) => {
+export const ImageBox: ParentComponent<Props> = (props) => {
     return (
         <Box
             direction="column"
             backgroundColor="#fff"
-            width={width}
-            height="100px"
+            width={props.width}
+            height="80px"
             useFlexGap
             spacing={5}
             {...animation().flip}
+            pointer
+            onClick={props.onClick}
+            sx="min-width: 70px; max-width: 200px;"
+            padding="1rem"
         >
-            <Image src={image} alt="Image" width={imgWidth} height={height} />
-            <Text>{children}</Text>
-            {hoverText && <Text>{hoverText}</Text>}
+            <Stack direction={!props.click ? 'column' : 'row'} useFlexGap spacing={4}>
+                <Image src={props.image} alt="Image" width={props.imgWidth} height={props.height} />
+                <Text fontSize="14px">{props.children}</Text>
+            </Stack>
+            {props.click && <Text fontSize="14px">{props.hoverText}</Text>}
         </Box>
     );
 };
