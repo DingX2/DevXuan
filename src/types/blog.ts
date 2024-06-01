d5b2719695c98e15259ac6336e2671cc6349b162
DingX2 <pourding@naver.com>
2024-06-02 03:35:17 +0900

    feat(blog): blog 타입 선언

---
+++ src/types/blog.ts
@@ -0,0 +1,8 @@
+export interface Blog {
+    id: number;
+    title: string;
+    subtitle: string;
+    content: string;
+    writeDate: string;
+    image?: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,4 @@ export * from './canvas';
 export * from './project';
 export * from './animation';
 export * from './skill';
+export * from './blog';
