b6f59d250aef13139b0a0b9793168757994a09e6
DingX2 <pourding@naver.com>
2024-06-02 01:00:29 +0900

    fix(speech): absolute 버그 수정

--- src/components/molecules/Speech.tsx
+++ src/components/molecules/Speech.tsx
@@ -1,34 +1,35 @@
 import type { ParentComponent } from 'solid-js';
 import { Image, Box, Text } from '@/components/atoms';
+import { type Style } from '@/types';
 
-interface Props {}
+interface Props extends Style {}
 
-const style = {
-    tail: /* css */ `
-        position: absolute;
-        top: 0;
-        left: -5px;
-        z-index: 10;
-    `,
+export const Speech: ParentComponent<Props> = (props) => {
+    const style = {
+        tail: /* css */ `
+            position: absolute;
+            top: 0;
+            left: -5px;
+            z-index: 10;
+        `,
 
-    box: /* css */ `
-        width: fit-content;
-        height: fit-content;
-        position: relative;
-        padding: 0.5rem 1rem;
-        margin-left: 1rem;
-        border-radius: 13px;
-        background-color: #efefef;
-        z-index: 11;
-    `,
-};
+        box: /* css */ `
+            width: fit-content;
+            height: fit-content;
+            position: relative;
+            padding: 0.5rem 1rem;
+            margin-left: 1rem;
+            border-radius: 13px;
+            background-color: #efefef;
+            z-index: 11;
+        `,
+    };
 
-export const Speech: ParentComponent<Props> = ({ children }) => {
     return (
         <Box sx={style.box}>
             <Image src="/image/speech.svg" width={24} height={24} useAbsolute sx={style.tail} />
             <Text fontSize="14px" sx="z-index:11;">
-                {children}
+                {props.children}
             </Text>
         </Box>
     );
