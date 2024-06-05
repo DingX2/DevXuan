9f933f4c3dddb11ef4d95c1461fbd39b94ae2058
DingX2 <pourding@naver.com>
2024-06-05 11:15:07 +0900

    feat(box): type, fitContent 속성 추가

--- src/components/atoms/Box.ts
+++ src/components/atoms/Box.ts
@@ -3,6 +3,7 @@ import { styled } from 'solid-styled-components';
 import type { Style } from '@/types';
 
 interface Props extends Style {
+    type?: 'whiteBox' | 'grayBox';
     width?: string;
     height?: string;
     backgroundColor?: string;
@@ -11,10 +12,12 @@ interface Props extends Style {
     useFlexGap?: boolean;
     padding?: string;
     pointer?: boolean;
+    fitContent?: boolean;
     onClick?: () => void;
 }
 
 /**
+ * @param {'whiteBox'|'grayBox'} type - type 종류
  * @param {string} width - 너비
  * @param {string} height - 높이
  * @param {string} backgroundColor - 배경색
@@ -23,9 +26,12 @@ interface Props extends Style {
  * @param {boolean} useFlexGap - gap 사용유무
  * @param {string} padding - padding
  * @param {boolean} pointer - pointer
+ * @param {boolean} fitContent - fitContent 사용유무
  * @param {() => void} onClick - 클릭이벤트
  */
 export const Box = styled(Motion.div)<Props>`
+    ${({ type }) => (type === 'whiteBox' ? `background-color: #fff; padding: 1rem;` : '')};
+    ${({ type }) => (type === 'grayBox' ? `background-color: #eee; padding: 0.5rem 1rem;` : '')};
     width: ${({ width }) => width || '100%'};
     height: ${({ height }) => height || '100%'};
     display: flex;
@@ -37,5 +43,6 @@ export const Box = styled(Motion.div)<Props>`
     ${({ padding }) => (padding ? `padding: ${padding};` : '')}
     ${({ useFlexGap, spacing }) => (useFlexGap ? `gap: ${spacing || 0}px; ` : `margin-top: ${spacing || 0}px;`)}
     ${({ pointer }) => (pointer ? `cursor: pointer;` : '')}
+    ${({ fitContent }) => (fitContent ? `height: fit-content; width: fit-content;` : '')}
     ${({ sx }) => sx}
 `;
