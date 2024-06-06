7a62fd3ae091db242adbdce2b6b8c92db1855d3a
DingX2 <pourding@naver.com>
2024-06-07 00:41:33 +0900

    feat(card): fitContent 속성 추가

--- src/components/molecules/Card.ts
+++ src/components/molecules/Card.ts
@@ -3,12 +3,13 @@ import { styled } from 'solid-styled-components';
 import type { Style } from '@/types';
 
 interface Props extends Style {
-    width?: number;
-    height?: number;
+    width?: number | string;
+    height?: number | string;
     top?: boolean;
     middle?: boolean;
     bottom?: boolean;
     shadow?: boolean;
+    fitContent?: boolean;
     borderColor?: string;
     padding?: string;
 }
@@ -16,9 +17,10 @@ interface Props extends Style {
 export const Card: ParentComponent<Props> = styled.div`
     display: flex;
     flex-direction: column;
-    width: ${({ width }) => width || 300}vw;
+    width: ${({ width }) => (typeof width === 'number' ? `${width}vw` : width)};
     max-width: 1080px;
-    height: ${({ height }) => height || 300}px;
+    height: ${({ height }) => (typeof height === 'number' ? `${height}vh` : height)};
+    height: ${({ fitContent }) => (fitContent ? 'fit-content' : 'auto')};
     ${({ padding }) => `padding: ${padding};`}
     background: rgba(255, 255, 255, 0.1);
     ${({ shadow }) => (shadow ? 'box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);' : '')};
