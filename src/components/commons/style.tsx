b3fec24252320fa107e3b371ad729fdbd4c8f4e3
DingX2 <pourding@naver.com>
2024-06-06 21:41:30 +0900

    style(style): font 설정

--- src/components/commons/style.tsx
+++ src/components/commons/style.tsx
@@ -8,11 +8,37 @@ export const GlobalStyle = () => {
         src: url('public/fonts/Pretendard-Regular.woff') format('woff');
     }
 
-    * {
-        font-family: 'Pretendard-Regular', Noto Sans KR, sans-serif;
+    @font-face {
+        font-family: 'Cafe24MeongiB';
+        font-display: swap;
+        src: url('public/fonts/Cafe24Meongi-B-v1.0.woff') format('woff');
+    }
+
+    @font-face {
+        font-family: 'Cafe24SsurroundAir';
+        font-display: swap;
+        src: url('public/fonts/Cafe24SsurroundAir-v1.1.woff') format('woff');
+    }
+
+    
+    @font-face {
+        font-family: 'Cafe24MeongiW';
+        font-display: swap;
+        src: url('public/fonts/Cafe24Meongi-W-v1.0.woff') format('woff');
+    }
+
+    @font-face {
+        font-family: 'Cafe24Decoshadow';
+        font-display: swap;
+        src: url('public/fonts/Cafe24Decoshadow.woff') format('woff');
+    }
+
+    body {
+        font-family: 'Pretendard-Regular', 'Noto Sans KR', sans-serif;
         font-weight: 400;
         font-style: normal;
     }
+
     `;
     return <GlobalStyleComponent />;
 };
