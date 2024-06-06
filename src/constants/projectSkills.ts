c60bd25fb9a3be2f602b65e477fd9d7b741fe305
DingX2 <pourding@naver.com>
2024-06-07 02:14:34 +0900

    chore(projectskills): 프로젝트 기술스택 작성

--- src/constants/projectSkills.ts
+++ src/constants/projectSkills.ts
@@ -2,26 +2,77 @@ import { type ProjectSkills } from '@/types';
 import { colors } from '@/constants/colors';
 
 export const projectSkills: ProjectSkills = {
-    blueMosaic: {
+    BlueMosaic: {
         skills: [
-            { text: 'Vite', color: colors.pink },
-            { text: 'Typescript', color: colors.pink },
-            { text: 'Zustand', color: colors.pink },
-            { text: 'Emotion', color: colors.pink },
+            { text: 'Vite', color: colors.blue },
+            { text: 'Typescript', color: colors.blue },
+            { text: 'Zustand', color: colors.blue },
             { text: 'React', color: colors.blue },
+            { text: 'Emotion', color: colors.pink },
+            { text: 'ESLint', color: colors.yellow },
             { text: 'Figma', color: colors.yellow },
         ],
-        roles: [{ text: 'FE', color: 'blue' }],
+        roles: [{ text: 'FE', color: colors.blue }],
     },
-    tadaktadak: {
+    TadakTadak: {
         skills: [
-            { text: 'Vite', color: 'pink' },
-            { text: 'Typescript', color: 'pink' },
-            { text: 'Zustand', color: 'pink' },
-            { text: 'Emotion', color: 'pink' },
-            { text: 'React', color: 'blue' },
-            { text: 'Figma', color: 'yellow' },
+            { text: 'Vite', color: colors.blue },
+            { text: 'Typescript', color: colors.blue },
+            { text: 'Zustand', color: colors.blue },
+            { text: 'react-query', color: colors.blue },
+            { text: 'mui', color: colors.pink },
+            { text: 'Emotion', color: colors.pink },
+            { text: 'ESLint', color: colors.yellow },
+            { text: 'prettier', color: colors.yellow },
+            { text: 'Figma', color: colors.yellow },
         ],
-        roles: [{ text: 'FE', color: 'blue' }],
+        roles: [{ text: 'FE', color: colors.blue }],
+    },
+    HappyNewHere: {
+        skills: [
+            { text: 'Vite', color: colors.blue },
+            { text: 'Typescript', color: colors.blue },
+            { text: 'Zustand', color: colors.blue },
+            { text: 'React', color: colors.blue },
+            { text: 'Emotion', color: colors.pink },
+            { text: 'Figma', color: colors.yellow },
+        ],
+        roles: [{ text: 'FE', color: colors.blue }],
+    },
+    AhMorningDiary: {
+        skills: [
+            { text: 'Vite', color: colors.blue },
+            { text: 'Javascript', color: colors.blue },
+            { text: 'redux/toolkit', color: colors.blue },
+            { text: 'React', color: colors.blue },
+            { text: 'sass', color: colors.pink },
+            { text: 'framer-motion', color: colors.pink },
+            { text: 'Figma', color: colors.yellow },
+        ],
+        roles: [{ text: 'FE', color: colors.blue }],
+    },
+    Udongsari: {
+        skills: [
+            { text: 'Vite', color: colors.blue },
+            { text: 'Javascript', color: colors.blue },
+            { text: 'redux/toolkit', color: colors.blue },
+            { text: 'ws', color: colors.blue },
+            { text: 'React', color: colors.blue },
+            { text: 'sass', color: colors.pink },
+            { text: 'Figma', color: colors.yellow },
+        ],
+        roles: [{ text: 'FE', color: colors.blue }],
+    },
+    ShareIT: {
+        skills: [
+            { text: 'Vite', color: colors.blue },
+            { text: 'Javascript', color: colors.blue },
+            { text: 'redux/toolkit', color: colors.blue },
+            { text: 'React', color: colors.blue },
+            { text: 'sass', color: colors.pink },
+            { text: 'Nginx', color: colors.yellow },
+            { text: 'AWS', color: colors.yellow },
+        ],
+        roles: [{ text: 'FE', color: colors.blue }],
     },
 };
