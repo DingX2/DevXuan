78cd72a7542815a289287e448d5bda957d428aed
DingX2 <pourding@naver.com>
2024-06-06 22:36:12 +0900

    feat(nav): mobile 버전 구현

--- src/components/molecules/Nav.tsx
+++ src/components/molecules/Nav.tsx
@@ -2,10 +2,12 @@ import { type Component, createMemo, For, Show } from 'solid-js';
 import { useLocation } from '@solidjs/router';
 import { Link, Stack, Box } from '@/components/atoms';
 import { links, mobileHome } from '@/constants';
+import { type Style } from '@/types';
 
-interface Props {
+interface Props extends Style {
     useAbsolute?: boolean;
     useBox?: boolean;
+    mobile?: boolean;
     hidden?: boolean;
 }
 
@@ -33,18 +35,18 @@ export const Nav: Component<Props> = (props) => {
         <>
             {!props.hidden && (
                 <>
-                    <Show when={!props.useBox}>
+                    <Show when={!props.useBox && !props.mobile}>
                         <Stack
                             direction="row"
                             spacing="30"
                             useFlexGap
                             center
-                            sx={`position: ${props.useAbsolute ? 'absolute' : 'relative'}; top: 0; z-index: 99; ${mobileHome.mobile}`}
+                            sx={`position: ${props.useAbsolute ? 'absolute' : 'relative'}; top: 0; z-index: 99; ${mobileHome.tablet} ${props.sx};`}
                         >
                             <NavLinks />
                         </Stack>
                     </Show>
-                    <Show when={props.useBox}>
+                    <Show when={props.useBox && !props.mobile}>
                         <Stack sx="align-items: center;">
                             <Box
                                 width="70%"
@@ -53,7 +55,20 @@ export const Nav: Component<Props> = (props) => {
                                 useFlexGap
                                 spacing={30}
                                 backgroundColor="#fff"
-                                sx={`position: relative; align-items: center; margin-top: 2rem; top: 0; z-index: 99; ${mobileHome.mobile} ${mobileHome.tablet}`}
+                                sx={`position: relative; align-items: center; margin-top: 2rem; top: 0; z-index: 99; ${mobileHome.mobile} ${mobileHome.tablet} ${props.sx}`}
+                            >
+                                <NavLinks />
+                            </Box>
+                        </Stack>
+                    </Show>
+                    <Show when={props.mobile}>
+                        <Stack sx="align-items: center;">
+                            <Box
+                                width="100%"
+                                height="40px"
+                                padding="1rem"
+                                backgroundColor="#fff"
+                                sx={`display: flex; justify-content: space-around; position: fixed; bottom: 0; z-index: 99; ${mobileHome.mobileNav} ${props.sx}`}
                             >
                                 <NavLinks />
                             </Box>
