544798347f4a78d4100f6043d8a6ea4c9f4acedd
DingX2 <pourding@naver.com>
2024-06-20 01:41:09 +0900

    feat(index): queryClient 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import { render } from 'solid-js/web';
+import { QueryClientProvider, QueryClient } from '@tanstack/solid-query';
 import { Router } from './Router';
 import './normalize.css';
 
-render(() => <Router />, document.getElementById('root')!);
+const queryClient = new QueryClient();
+
+render(
+    () => (
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+    ),
+    document.getElementById('root')!,
+);
