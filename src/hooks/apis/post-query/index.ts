ebd5f2b6d19b13a64c5e9f6f504375539e733eec
DingX2 <pourding@naver.com>
2024-06-20 01:39:54 +0900

    feat(index): solidquery문 작성

--- src/hooks/apis/post-query/index.ts
+++ src/hooks/apis/post-query/index.ts
@@ -1,1 +1,26 @@
-export * from './respository';
+import { createQuery } from '@tanstack/solid-query';
+import { QUERY_OPTIONS } from '@/constants';
+import { PostRepository } from './repository';
+
+const queryKey = {
+    all: ['post'],
+    checkHello() {
+        return [...this.all, 'hello'];
+    },
+};
+
+export const useGetPost = () => {
+    return createQuery(() => ({
+        queryKey: queryKey.all,
+        queryFn: async () => PostRepository.getPost(),
+        ...QUERY_OPTIONS,
+    }));
+};
+
+export const useHello = () => {
+    return createQuery(() => ({
+        queryKey: queryKey.checkHello(),
+        queryFn: async () => PostRepository.hello(),
+        ...QUERY_OPTIONS,
+    }));
+};
