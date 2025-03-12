e430d035c1c4627703c84743c3bb6fd285ffab1e
DevXuan <pourding@naver.com>
2025-03-12 20:19:10 +0900

    refactor(projectSkills.ts): projectSkill 추가 (Solid.js 포트폴리오)

--- src/constants/projectSkills.ts
+++ src/constants/projectSkills.ts
@@ -2,6 +2,26 @@ import { type ProjectSkills } from '@/types';
 import { colors } from '@/styles';
 
 export const projectSkills: ProjectSkills = {
+    Portfolio: {
+        skills: [
+            { text: 'Vite', color: colors.blue },
+            { text: 'Typescript', color: colors.blue },
+            { text: 'Solid.js', color: colors.blue },
+            { text: 'Next.js', color: colors.blue },
+            { text: 'Styled-components', color: colors.pink },
+            { text: 'Ky', color: colors.pink },
+            { text: 'Tanstack Query', color: colors.pink },
+            { text: 'ESLint', color: colors.yellow },
+            { text: 'husky', color: colors.yellow },
+            { text: 'lint-staged', color: colors.yellow },
+            { text: 'Prettier', color: colors.yellow },
+            { text: 'Figma', color: colors.yellow },
+        ],
+        roles: [
+            { text: 'FE', color: colors.blue },
+            { text: 'BE', color: colors.pink },
+        ],
+    },
     BlueMosaic: {
         skills: [
             { text: 'Vite', color: colors.blue },
