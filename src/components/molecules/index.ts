9efcce8601329e25520285c1ddc668540ff17438
DevXuan <pourding@naver.com>
2025-08-05 00:21:18 +0900

    refactor(index.ts): simplecursor barrel file add

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -8,4 +8,6 @@ export * from './Canvas';
 export * from './ColorBoxList';
 export * from './CardContent';
 export * from './CardHeader';
+export * from './ScrollFloatText';
+export * from './SimpleCursor';
 export * from './Skeleton';
