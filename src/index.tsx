1537a10d8d5d0cb362e38b6beb4130dd3324213b
DevXuan <pourding@naver.com>
2026-03-31 01:35:09 +0900

    refactor(CareerTemplate.tsx): animatedreveal 컴포넌트 및 text 컴포넌트 적용 리팩토링

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from 'solid-js/web';
 import { QueryClientProvider, QueryClient } from '@tanstack/solid-query';
 import { Router } from './Router';
+import '@/styles/tokens/token.css';
 import './normalize.css';
 
 const queryClient = new QueryClient();
